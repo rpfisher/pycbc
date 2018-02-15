fcf6d5606e25dd063fcd4696f1a91b317eb8a938
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-15 07:56:56 +0000

    Deploy ligo-cbc/pycbc to github.com/ligo-cbc/pycbc.git:gh-pages

---
+++ latest/html/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'e71541',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
